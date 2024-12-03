9beddce867963bc91a382e4bf6c67be20aa2fbcf
Anushka Panicker <anupanicker1917@gmail.com>
2024-12-04 00:38:32 +0530

    feat: setup initial auth and root pages plus routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import SignInForm from "./_auth/forms/SignInForm";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import { Home } from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-in" element={<SignUpForm />} />
+        </Route>
+
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
